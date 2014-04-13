7c28ce041ea5ccc9c9559007f50530027ce1d3f0
root <root@asterix.puppetlabs.vm>
2014-04-13 00:17:27 +0000

    Edited class spec file to make it work

--- spec/classes/torrentexpander_spec.rb
+++ spec/classes/torrentexpander_spec.rb
@@ -1,54 +1,57 @@
 require 'spec_helper'
 
 describe 'torrentexpander', :type => :class do
-  let :facts do
-  {
-  :kernel => 'Linux',
-  }
-  end
-  let :pre_condition do
-    'include torrentexpander::params'
-  end
-  describe "installing torrentexpander with defaults" do
-    context "installing into /root/bin" do
-      let :params do
-        {
-          :install_dir => '/root/bin',
-        }
-      end
-      it { should contain_class('torrentexpander') }
-      it { should contain_package('rar') }
-      it { should contain_file("/root/bin/torrentexpander.sh").with(
-        'ensure' => 'file',
-        'owner'  => 'root',
-        'group'  => 'root'
-      ) }
-      it { should contain_file("/root/bin/torrentexpander_settings.ini").with(
-        'ensure' => 'file',
-        'owner'  => 'root',
-        'group'  => 'root'
-      ) }
+  context "on Linux system to" do
+    let :facts do
+    {
+    :kernel => 'Linux',
+    }
     end
-  end
-  describe "installing torrentexpander with custom user/group" do
-    context "user/group torrent" do
-      let :params do
-      {
-        :install_dir => '/root/bin',
-        :user        => 'torrent',
-        :group       => 'torrent',
-      }
-      it { should contain_file("/root/bin/torrentexpander.sh") }
-      it { should contain_file("/root/bin/torrentexpander.sh").with(
-        'ensure' => 'file',
-        'owner'  => 'torrent',
-        'group'  => 'torrent'
-      ) }
-      it { should contain_file("/root/bin/torrentexpander_settings.ini").with(
-        'ensure' => 'file',
-        'owner'  => 'torrent',
-        'group'  => 'torrent'
-      ) }
+    let :pre_condition do
+      'include torrentexpander::params'
+    end
+    describe "install with defaults" do
+      context "into /root/bin" do
+        let :params do
+          {
+            :install_dir => '/root/bin',
+          }
+        end
+        it { should contain_class('torrentexpander') }
+        it { should contain_package('rar') }
+        it { should contain_file("/root/bin/torrentexpander.sh").with(
+          'ensure' => 'file',
+          'owner'  => 'root',
+          'group'  => 'root'
+        ) }
+        it { should contain_file("/root/bin/torrentexpander_settings.ini").with(
+          'ensure' => 'file',
+          'owner'  => 'root',
+          'group'  => 'root'
+        ) }
+      end
+    end
+    describe "install with custom user and group" do
+      context "using user/group torrent" do
+        let :params do
+          {
+            :install_dir => '/root/bin',
+            :user        => 'torrent',
+            :group       => 'torrent',
+          }
+        end
+        it { should contain_class('torrentexpander') }
+        it { should contain_package('rar') }
+        it { should contain_file("/root/bin/torrentexpander.sh").with(
+          'ensure' => 'file',
+          'owner'  => 'torrent',
+          'group'  => 'torrent'
+        ) }
+        it { should contain_file("/root/bin/torrentexpander_settings.ini").with(
+          'ensure' => 'file',
+          'owner'  => 'torrent',
+          'group'  => 'torrent'
+        ) }
       end
     end
   end
